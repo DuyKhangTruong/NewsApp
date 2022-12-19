ae8149a270dbbc5cf39deac900e33a451028c760
Khang Nguyen <36774417+DuyKhangTruong@users.noreply.github.com>
2022-12-19 13:01:55 -0800

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Duy Khang Nguyen Truong on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
